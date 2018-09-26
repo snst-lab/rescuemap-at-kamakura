0483a031bd2d7981f356c430cb6645b4be6a810b
snst-lab <40202502+snst-lab@users.noreply.github.com>
2018-09-26 19:35:19 +0900

    Delete deploy.bat

--- deploy.bat
+++
@@ -1,6 +0,0 @@
-git config user.name "snst-lab"
-git remote rm origin
-git remote add origin git@github.com:snst-lab/rescuemap-at-kamakura.git
-git add .
-git commit -a -m "update"
-git push origin master
